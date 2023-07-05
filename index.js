208e61038a734c74260ae1a486ffdcca8fc7c0ba
Jenipher Oduor <jenipheroduor@gmail.com>
2023-07-05 09:04:02 +0300

    Yet to check tests

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1;
+const num2;
+const multiply = num1 * num2;
 
 // Write your code here
+
+Math.random();
+
+const num3 = 16;
+const num4 = 6;
+
+const mod = num3 % num4;
+
+Math.max(5,7,20) = 20;
