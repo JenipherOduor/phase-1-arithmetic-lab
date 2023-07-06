fc88a7db2e21eb7f301c2f9a76e8597f6b07db70
Jenipher Oduor <jenipheroduor@gmail.com>
2023-07-06 12:01:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const num1;
-const num2;
+const num1 = 31;
+const num2 = 2;
 const multiply = num1 * num2;
 
 // Write your code here
 
-Math.random();
+const random = Math.random();
 
 const num3 = 16;
 const num4 = 6;
 
 const mod = num3 % num4;
 
-Math.max(5,7,20) = 20;
+
+const max  = Math.max(5,7,20);
